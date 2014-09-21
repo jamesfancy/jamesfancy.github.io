c82b616f9add9f99877d6425ed746d9b68ae37b5
jamesfancy <jamesfancy@126.com>
2014-09-21 12:23:24 +0800

    highlight codes in the blog

--- libs/page/blog/issue.js
+++ libs/page/blog/issue.js
@@ -4,25 +4,34 @@ define(function(require, exports, module) {
     var URL_BASE = "https://github.com/jamesfancy/jamesfancy.github.io/issues/"
 
     var app = require("app")
-    var github = app.github;
-
+    require("shjs")
     var Md2Html = require("./md2html")
 
-    require("shjs")
-    function highlight() {
-        sh_highlightDocument("lang/", ".min.js")
-    }
+    var github = app.github;
 
     var blog = {
         title: $("#blog-title"),
         content: $("#blog-content"),
         comments: $("#comments"),
+        highlight: function() {
+            this.content.find("pre").each(function() {
+                var pre = $(this)
+                var code = pre.children("code")
+                var lang = code.attr("class")
+                if (lang && lang.match(/language\-/)) {
+                    pre.addClass(lang.replace(/language\-/, "sh_"))
+                }
+            })
+
+            sh_highlightDocument("../libs/lang/", ".min.js")
+        },
         render: function(id, issue) {
             this.title.text(issue.title || "标题去哪儿啦——？")
             this.content.empty().append(new Md2Html(issue.body).toHtml())
             this.comments
                 .text(issue.comments.toString() + " 条评论")
                 .attr("href", URL_BASE + id + "#new_comment_field")
+            this.highlight()
         }
     }
 
