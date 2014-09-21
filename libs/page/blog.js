3cfdf876cd64fe33eaa1aad2dd0a1ad5bcbba12d
jamesfancy <jamesfancy@126.com>
2014-09-21 08:52:47 +0800

    处理菜单的 a 的点击事件，如果是页面内跳转，由脚本控制，不由浏览器控制。

--- libs/page/blog.js
+++ libs/page/blog.js
@@ -1,18 +1,32 @@
-/* jshing asi: true */
+/* jshint asi: true */
 /* global define: false */
-define(function(require, exports, module) {
+define(function(require) {
     require("jquery")
     var $ = jQuery
-
-    $(document).on("selectstart", false)
 
     $(window).on("hashchange", function() {
         var hash = location.hash
         var id = hash.match(/^#(.*)/) ? RegExp.$1 : ""
         if (id) {
-            require("./blog/issue").show(id)
+            if (id.match(/^\d+$/)) {
+                // only number hash is blog id
+                require("./blog/issue").show(id)
+            }
         } else {
             require("./blog/list").show()
         }
     }).trigger("hashchange")
+
+    $(document).on("selectstart", false)
+
+    var scrollPanel = $("#blog-content").parent()
+    $("#blog-content").on("click", "a", function(e) {
+        var href = $(this).attr("href")
+        if (href.match(/^#\d+$/)) {
+            return
+        }
+
+        e.preventDefault()
+        scrollPanel.scrollTop(scrollPanel.scrollTop() + $(href).position().top - 32)
+    })
 })
